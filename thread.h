820c40a881884914664b2a7d58213af8d9252c65
akabigbro <akabigbro@0b11e882-61a5-11de-8834-e56a46bc7112>
2009-08-26 20:15:12 +0000

    merging stapi.20090625

--- thread.h
+++ thread.h
@@ -6,25 +6,25 @@
 
 class Thread
 {
-private:
-    int error;
-    static void * EntryPoint(void * arg);
-protected:
-    bool Terminated;
-    pthread_t threadId;
+    private:
+        int error;
+        static void * EntryPoint(void * arg);
+    protected:
+        bool Terminated;
+        pthread_t threadId;
 
-    virtual void Execute(void)=0;
-public:
-    Thread(void);
-    ~Thread(void);
+        virtual void Execute(void)=0;
+    public:
+        Thread(void);
+        ~Thread(void);
 
-    void Start(void) throw(int&);
-    void Detach(void) throw(int&);
-    void Join(void) throw(int&);
-    void Cancel(void) throw(int&);
-    void Kill(int sig = SIGKILL) throw(int&);
-    void Terminate(void);
-    bool IsRunning(void);
+        void Start(void) throw(int&);
+        void Detach(void) throw(int&);
+        void Join(void) throw(int&);
+        void Cancel(void) throw(int&);
+        void Kill(int sig = SIGKILL) throw(int&);
+        void Terminate(void);
+        bool IsRunning(void);
 };
 
 #endif//THREAD_H
